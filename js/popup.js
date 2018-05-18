c05d461ad5d835a7c23d42a7e5ef1581fb83e74d
Simon <fessehay@ualberta.ca>
2018-05-18 03:11:26 -0600

    add event delete/copy

--- js/popup.js
+++ js/popup.js
@@ -1,10 +1,28 @@
 var ggAPI = "https://api.smash.gg/tournament/";
 var db = new PouchDB('smashCalendar');
 
+const copyToClipboard = str => {
+  const el = document.createElement('textarea');
+  el.value = str;
+  el.setAttribute('readonly', '');
+  el.style.position = 'absolute';
+  el.style.left = '-9999px';
+  document.body.appendChild(el);
+  const selected =
+    document.getSelection().rangeCount > 0 ? document.getSelection().getRangeAt(0) : false;
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+  if (selected) {
+    document.getSelection().removeAllRanges();
+    document.getSelection().addRange(selected);
+  }
+};
+
 var app = new Vue({
   el: '#app',
   data: {
-    eventInput: 'pulsar-premier-league',
+    eventInput: 'pound-underground',
     addModal: false,
     edit:true,
     events:[],
@@ -13,32 +31,42 @@ var app = new Vue({
 
   methods: {
     addEvent:function () {
-    var self = this;
-    axios.get('https://api.smash.gg/tournament/' + this.eventInput)
-    .then(function (response) {
-        var tourney = response.data.entities.tournament;
-        var logo = tourney.images.filter((img) => {
-            return img.type == "profile"
-        })[0];
-        var doc = {
-            _id: tourney.shortSlug,
-            name: tourney.name,
-            startAt: tourney.startAt,
-            endAt: tourney.endAt,
-            city: tourney.city,
-            state: tourney.addrState,
-            logo: logo.url,
-            registrationClosesAt: tourney.registrationClosesAt
-        };
-        // db.put(doc);
-        self.events.push(doc);
-        self.addModal = false;
+        var self = this;
+        axios.get('https://api.smash.gg/tournament/' + this.eventInput)
+        .then(function (response) {
+            var tourney = response.data.entities.tournament;
+            var logo = tourney.images.filter((img) => {
+                return img.type == "profile"
+            })[0];
+            var doc = {
+                _id: tourney.shortSlug,
+                name: tourney.name,
+                startAt: tourney.startAt * 1000,
+                endAt: tourney.endAt * 1000,
+                city: tourney.city,
+                state: tourney.addrState,
+                logo: logo.url,
+                slug: tourney.slug,
+                registrationClosesAt: tourney.registrationClosesAt
+            };
+            // db.put(doc);
+            self.events.push(doc);
+            self.addModal = false;
+            document.getElementById('navMenu').classList.remove('is-active');
+            document.querySelectorAll('.navbar-burger')[0].classList.remove('is-active');
 
-    })
-      .catch(function (error) {
-        console.log("Error could not add event:" + error);
+        })
+        .catch(function (error) {
+            console.log("Error could not add event:" + error);
+        });
+    },
 
-      });
+    copyEvent: function (event) {
+        copyToClipboard("https://smash.gg/" + event.slug );
+    },
+
+    deleteEvent: function (index) {
+        this.events.splice(index, 1);
     },
 
     toggleAddModal : function() {
@@ -48,7 +76,7 @@ var app = new Vue({
 
   filters: {
     moment: function (date) {
-      return moment(date).format('MMMM Do YYYY, h:mm:ss a');
+      return moment(date).format('MMMM Do');
     }
   }
 
