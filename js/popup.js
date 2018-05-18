b0f70963877d3ba834d9124fd62ea5ff0e516373
Simon <fessehay@ualberta.ca>
2018-05-18 14:57:19 -0600

    storage and permissions set up

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,4 @@
 var ggAPI = "https://api.smash.gg/tournament/";
-var db = new PouchDB('smashCalendar');
 
 const copyToClipboard = str => {
   const el = document.createElement('textarea');
@@ -27,6 +26,33 @@ var app = new Vue({
     edit:true,
     events:[],
     loading:false,
+    tabs:{
+        allEvents: true,
+        prevEvents: false,
+        upcomingEvents: false
+    }
+  },
+
+  created : function() {
+    chrome.storage.sync.get(["smashCalendar"], (result) => {
+      if(result.smashCalendar){
+        this.events = result.smashCalendar;
+      }
+    });
+
+    chrome.tabs.getSelected(null, function(tab) {
+        tabUrl = tab.url;
+        ggPage = tabUrl.includes('smash.gg/');
+        if(ggPage){
+            var opt = {
+                type: 'list',
+                title: 'Visiting Event Page',
+                message: 'Primary message to display',
+                priority: 1,
+            };
+            chrome.notifications.create('notify1', opt, function(id) { console.log("Last error:", chrome.runtime.lastError); });
+        }
+    });
   },
 
   methods: {
@@ -50,10 +76,16 @@ var app = new Vue({
                 registrationClosesAt: tourney.registrationClosesAt
             };
             // db.put(doc);
+            
             self.events.push(doc);
+            self.events = self.events.sort((a, b) => a.startAt > b.startAt );
             self.addModal = false;
             document.getElementById('navMenu').classList.remove('is-active');
             document.querySelectorAll('.navbar-burger')[0].classList.remove('is-active');
+
+            chrome.storage.sync.set({"smashCalendar": self.events}, function() {
+              console.log('Saved');
+            });
 
         })
         .catch(function (error) {
@@ -67,19 +99,58 @@ var app = new Vue({
 
     deleteEvent: function (index) {
         this.events.splice(index, 1);
+        chrome.storage.sync.set({"smashCalendar": this.events}, function() {
+          console.log('Saved');
+        });
     },
 
     toggleAddModal : function() {
         this.addModal = !this.addModal;
     },
+
+    changeTab: function(tabName) {
+        Object.keys(this.tabs).forEach((key,index) => {
+            if(tabName == key) {
+                this.tabs[key] = true;
+            }
+            else {
+                this.tabs[key] = false;
+            }
+        });
+    },
+
+    github: function() {
+        chrome.tabs.create({url: "https://github.com/fessehaye/Smash-Calendar-Extension"});
+    },
+
+    filteredEvents: function () {
+      // `this` points to the vm instance
+      if(this.tabs.prevEvents) {
+        return this.events.filter((e) => {
+            var now = new Date();
+            var event = new Date(e.endAt);
+            return event < now;
+        })
+      }
+      else if(this.tabs.upcomingEvents) {
+        return this.events.filter((e) => {
+            var now = new Date();
+            var event = new Date(e.endAt);
+            return event > now;
+        })
+      }
+      else {
+          return this.events;
+      }
+
+    }
   },
 
   filters: {
     moment: function (date) {
       return moment(date).format('MMMM Do');
     }
-  }
-
+  },
   
 
 });
